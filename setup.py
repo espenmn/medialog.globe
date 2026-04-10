aaa2060b5419e19fbb036e5cc4e142e7679b4a57
Espen Moe-Nilssen <espen@medialog.no>
2026-04-10 13:54:38 +0200

    version change

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -46,7 +48,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires=">=3.6",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
