fd2f8091d693ace352f2ed9a54e22cc6b69944cc
Espen Moe-Nilssen <espen@medialog.no>
2026-05-27 10:27:57 +0200

    working content_listing

--- src/medialog/globe/views/globe_view.py
+++ src/medialog/globe/views/globe_view.py
@@ -5,11 +5,7 @@ from Products.Five.browser import BrowserView
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+class GlobeView(BrowserView):
 
-#class AnsatteView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('ansatte_view.pt')
-
-#    def get_items(self):
-#        return self.context.portal_catalog(portal_type='ansatt')
+    def results(self):
+        return self.context.results()
